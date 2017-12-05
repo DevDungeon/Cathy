28908293727c4042b082a3f3d75ab5e5d8ef389f
NanoDano <nanodano@devdungeon.com>
2017-12-04 21:45:17 -0600

    Minor structure tweaks

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ChattyCathy
 Artificial intelligence (AI) chat bot for Discord written in Python
+
+
+## Installation
+
+    pip install cathy
+    
+## Running
+
+    cathy --help
+    
+## Contact
+
+nanodano@devdungeon.com
+
+## Changelog
+
+* 0.1.1 - Creating initial project structure
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='cathy',
-      version='0.1.1',
+      version='0.1.2',
       description='Discord chat bot using AIML artificial intelligence..',
       url='https://github.com/NanoDano/ChattyCathy',
-      author='NanoDano',
+      author='DevDungeon',
       author_email='nanodano@devdungeon.com',
       license='GPL-3.0',
       packages=['cathy'],  # The folder with the real code
       scripts=['bin/cathy'],  # List of executable scripts to include
       zip_safe=False,
-      # install_requires=['']
+      install_requires=['docopt']
       )
