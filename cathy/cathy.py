aa2e106bb411fd9539f75ee18a01a932dc062c49
NanoDano <nanodano@devdungeon.com>
2017-12-04 21:25:44 -0600

    Initial project structure

---
+++ cathy/cathy.py
@@ -0,0 +1,14 @@
+
+
+# Load AIML
+
+# Connect to discord
+
+# Listen and chat
+
+class ChattyCathy():
+    def __init__(self):
+        print("Cathy initializing...")
+
+    def run(self):
+        print("Running...")
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='cathy',
+      version='0.1.1',
+      description='Discord chat bot using AIML artificial intelligence..',
+      url='https://github.com/NanoDano/ChattyCathy',
+      author='NanoDano',
+      author_email='nanodano@devdungeon.com',
+      license='GPL-3.0',
+      packages=['cathy'],  # The folder with the real code
+      scripts=['bin/cathy'],  # List of executable scripts to include
+      zip_safe=False,
+      # install_requires=['']
+      )
