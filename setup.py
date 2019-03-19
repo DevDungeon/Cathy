e08041e39456c6987068d83ad6782a3af592e742
NanoDano <nanodano@devdungeon.com>
2019-03-19 00:25:43 -0500

    Roll version number

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+
+def readme_file_contents():
+    with open('README.md') as f:
+        data = f.read()
+    return data
+
+
 setup(
     name='cathy',
-    version='1.1.0',
+    version='1.1.1',
     description='Discord chat bot using AIML artificial intelligence.',
+    long_description=readme_file_contents(),
     url='https://github.com/DevDungeon/ChattyCathy',
     author='DevDungeon',
     author_email='nanodano@devdungeon.com',
