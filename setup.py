0c431c220c715195e5535a5c20c54600aad07e69
John Leon <nanodano@devdungeon.com>
2020-03-18 20:50:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cathy',
     version='3.0.3',
     description='Discord chat bot using AIML artificial intelligence.',
-    long_description="See https://github.com/DevDungeon/cathy,
+    long_description="See https://github.com/DevDungeon/cathy",
     url='https://github.com/DevDungeon/ChattyCathy',
     author='DevDungeon',
     author_email='nanodano@devdungeon.com',
