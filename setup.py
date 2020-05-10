b9aa1eed084e1d47efc233411f9d1761bbf6bc5e
nanodano <nanodano@devdungeon.com>
2020-05-10 09:18:41 +0000

    rolling verison num

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cathy',
-    version='3.4.0',
+    version='3.5.0',
     description='Discord chat bot using AIML artificial intelligence.',
     long_description="See https://github.com/DevDungeon/cathy",
     url='https://github.com/DevDungeon/ChattyCathy',
